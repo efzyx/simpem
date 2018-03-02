a0fa592a340adf7e06b384029e0563588affaa81
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2018-03-03 02:02:03 +0700

    mulai dari 0

--- resources/views/layouts/menu.blade.php
+++
@@ -1,8 +0,0 @@
-<li class="{{ Request::is('produks*') ? 'active' : '' }}">
-    <a href="{!! route('produks.index') !!}"><i class="fa fa-edit"></i><span>Produks</span></a>
-</li>
-
-<li class="{{ Request::is('pemesanans*') ? 'active' : '' }}">
-    <a href="{!! route('pemesanans.index') !!}"><i class="fa fa-edit"></i><span>Pemesanans</span></a>
-</li>
-
