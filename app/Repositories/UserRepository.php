d46a23e9452f26b943bcb9e71b08157cea213d2b
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2018-03-11 03:13:39 +0700

    hmmm?

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use App\User;
+use InfyOm\Generator\Common\BaseRepository;
+
+/**
+ * Class UserRepository
+ * @package App\Repositories
+ * @version March 10, 2018, 2:20 am WIB
+ *
+ * @method User findWithoutFail($id, $columns = ['*'])
+ * @method User find($id, $columns = ['*'])
+ * @method User first($columns = ['*'])
+*/
+class UserRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return User::class;
+    }
+}
---
+++ database/migrations/2018_03_11_005704_create_bahan_baku_histories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBahanBakuHistoriesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bahan_baku_histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('bahan_baku_id')->unsigned();
+            $table->integer('type');
+            $table->integer('pengadaan_id')->unsigned()->nullable();
+            $table->integer('produksi_id')->unsigned()->nullable();
+            $table->integer('opname_id')->unsigned()->nullable();
+            $table->integer('total_sisa');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('bahan_baku_id')->references('id')->on('bahan_bakus');
+            $table->foreign('pengadaan_id')->references('id')->on('pengadaans');
+            $table->foreign('produksi_id')->references('id')->on('produksis');
+            $table->foreign('opname_id')->references('id')->on('opnames');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('bahan_baku_histories');
+    }
+}
