b3d3e5fea9c5c220148bd98ca63ede5b2761c0d0
efzet <fauzi.padlaw@gmail.com>
2018-03-20 04:36:33 +0700

    fixed responsive datatable

--- resources/views/pemesanans/table.blade.php
+++ resources/views/pemesanans/table.blade.php
@@ -1,51 +1,45 @@
-<table class="table table-responsive" id="pemesanans-table">
+<div class="table-responsive">
+  <table class="table" id="pemesanans-table">
     <thead>
-        <tr>
-          <th width="5px">#</th>
-          <th>Pemesan</th>
-          <th>Produk</th>
-          <th>Tanggal</th>
-          <th>Status</th>
-          <th>Action</th>
-        </tr>
+      <tr>
+        <th width="15vw">#</th>
+        <th width="100vw">Pemesan</th>
+        <th width="100vw">Produk</th>
+        <th width="100vw">Tanggal</th>
+        <th width="100vw">Status</th>
+        <th width="100vw">Action</th>
+      </tr>
     </thead>
     <tbody>
-      @php
-        $no = 1;
-        $status = [
-          'Produksi',
-          'Sedang dikirim',
-          'Terkirim'
-        ];
-      @endphp
-    @foreach($pemesanans as $pemesanan)
-        <tr>
-            <td width="5px">{{ $no++ }}</td>
-            <td>{!! $pemesanan->nama_pemesanan !!}</td>
-            <td>{!! $pemesanan->produk->mutu_produk !!}</td>
-            <td>{!! $pemesanan->tanggal_pesanan->format('d F y') !!}</td>
-            <td>{!! $status[$pemesanan->status] !!}</td>
-            <td>
-                {!! Form::open(['route' => ['pemesanans.destroy', $pemesanan->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('pemesanans.produksis.index', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-refresh"></i></a>
-                    <a href="{!! route('pemesanans.show', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('pemesanans.edit', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
+      @php $no = 1; $status = [ 'Produksi', 'Sedang dikirim', 'Terkirim' ]; @endphp @foreach($pemesanans as $pemesanan)
+      <tr>
+        <td width="15vw">{{ $no++ }}</td>
+        <td width="100vw">{!! $pemesanan->nama_pemesanan !!}</td>
+        <td width="100vw">{!! $pemesanan->produk->mutu_produk !!}</td>
+        <td width="100vw">{!! $pemesanan->tanggal_pesanan->format('d F y') !!}</td>
+        <td width="100vw">{!! $status[$pemesanan->status] !!}</td>
+        <td width="100vw">
+          {!! Form::open(['route' => ['pemesanans.destroy', $pemesanan->id], 'method' => 'delete']) !!}
+          <div class='btn-group'>
+            <a href="{!! route('pemesanans.produksis.index', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-refresh"></i></a>
+            <a href="{!! route('pemesanans.show', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
+            <a href="{!! route('pemesanans.edit', [$pemesanan->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a> {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger
+            btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+          </div>
+          {!! Form::close() !!}
+        </td>
+      </tr>
+      @endforeach
     </tbody>
     <tfoot>
       <tr>
-        <th width="5px"></th>
-        <th><input type="text" placeholder="Search Pemesan" /></th>
-        <th><input type="text" placeholder="Search Produk" /></th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th width="15vw"></th>
+        <th width="100vw"><input class="col-sm-12" type="text" placeholder="Pemesan" /></th>
+        <th width="100vw"><input class="col-sm-12" type="text" placeholder="Produk" /></th>
+        <th width="100vw"><input class="col-sm-12" type="text" placeholder="Tanggal" /></th>
+        <th width="100vw"></th>
+        <th width="100vw"></th>
       </tr>
     </tfoot>
-</table>
+  </table>
+</div>
