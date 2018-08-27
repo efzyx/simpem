145599e95ac74d504f989fa281e37ed9f9ee5306
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2018-08-27 18:45:06 +0000

    Update resources/views/produksis/pdf.blade.php

--- resources/views/produksis/pdf.blade.php
+++ resources/views/produksis/pdf.blade.php
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <title>Cetak BPO Sheet</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+      .table{
+        font-size: 9px;
+      }
+    </style>
   </head>
   <body>
    <div class="content-wrapper">
